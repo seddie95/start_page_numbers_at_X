d3d70b03cc46c4c85b9a4344db13ea2bfdf93d95
sed <sed@live.ie>
2020-09-29 11:41:32 +0100

    Add Multiple templates

---
+++ pages/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import WordDoc
+
+
+class FileForm(forms.ModelForm):
+    doc_file = forms.FileField(label='', widget=forms.FileInput(
+        attrs={
+            "accept": ".docx,.doc"}
+    ))
+
+    class Meta:
+        model = WordDoc
+        fields = ('doc_file',)
