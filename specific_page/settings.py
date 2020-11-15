3a016f731bb6fa42076e776bf0cd645e0881ee8a
sed <sed@live.ie>
2020-11-15 21:02:34 +0000

    Update styling add responsive navbar

--- specific_page/settings.py
+++ specific_page/settings.py
@@ -23,12 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config['SECRET_KEY']
 SECRET_KEY = '23e40wdhcxsnkd0qwd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['23.96.82.252','www.numberingat.com','numberingat.com','*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
